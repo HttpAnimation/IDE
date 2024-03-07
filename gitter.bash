c8c8a726a0216ff1fe34684a7a07271a90147a08
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-07 14:17:33 -0500

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -1,15 +1,15 @@
 # Put gitter here
-mkdir IDE
-cd IDE
-git clone -b stable https://github.com/HttpAnimation/IDE.git
-mv IDE stable
-git clone -b blank https://github.com/HttpAnimation/IDE.git
-mv IDE blank 
-git clone -b extra https://github.com/HttpAnimation/IDE.git
-mv IDE extra
-git clone -b gh-pages https://github.com/HttpAnimation/IDE.git
-mv IDE gh-pages
-git clone -b gitter https://github.com/HttpAnimation/IDE.git
-mv IDE gitter
-git clone -b main https://github.com/HttpAnimation/IDE.git
-mv IDE main
+mkdir Kilt
+cd Kilt
+git clone -b stable https://github.com/HttpAnimation/Kilt.git
+mv Kilt stable
+git clone -b blank https://github.com/HttpAnimation/Kilt.git
+mv Kilt blank 
+git clone -b extra https://github.com/HttpAnimation/Kilt.git
+mv Kilt extra
+git clone -b gh-pages https://github.com/HttpAnimation/Kilt.git
+mv Kilt gh-pages
+git clone -b gitter https://github.com/HttpAnimation/Kilt.git
+mv Kilt gitter
+git clone -b main https://github.com/HttpAnimation/Kilt.git
+mv Kilt main
