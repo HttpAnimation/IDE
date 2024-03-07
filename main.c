2479f926fc5d5254c53e7cf1e221320f004f86c6
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-07 14:11:33 -0500

    sidebar

--- main.c
+++ main.c
@@ -15,16 +15,26 @@ int main(int argc, char *argv[]) {
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     g_signal_connect(window, "destroy", G_CALLBACK(on_window_closed), NULL);
 
+    // Create a vertical box container for the sidebar and text editor
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    // Create a label for displaying the file name
+    GtkWidget *file_label = gtk_label_new("File: None");
+    gtk_box_pack_start(GTK_BOX(vbox), file_label, FALSE, FALSE, 0);
+
     // Create a text view for editing code
     GtkWidget *text_view = gtk_text_view_new();
     GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
 
     // Create a scrolled window to contain the text view
     GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
+                                   GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
 
-    // Add the scrolled window to the main window
-    gtk_container_add(GTK_CONTAINER(window), scrolled_window);
+    // Add the scrolled window to the vertical box container
+    gtk_box_pack_start(GTK_BOX(vbox), scrolled_window, TRUE, TRUE, 0);
 
     // Show all widgets
     gtk_widget_show_all(window);
