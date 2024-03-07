6c43b48012ff70e4ec750c4ef3f2b0424c772a5e
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-07 14:06:57 -0500

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -1,15 +1,15 @@
 # Put gitter here
-mkdir NAME
-cd NAME
-git clone -b stable https://github.com/HttpAnimation/NAME.git
-mv NAME stable
-git clone -b blank https://github.com/HttpAnimation/NAME.git
-mv NAME blank 
-git clone -b extra https://github.com/HttpAnimation/NAME.git
-mv NAME extra
-git clone -b gh-pages https://github.com/HttpAnimation/NAME.git
-mv NAME gh-pages
-git clone -b gitter https://github.com/HttpAnimation/NAME.git
-mv NAME gitter
-git clone -b main https://github.com/HttpAnimation/NAME.git
-mv NAME main
+mkdir IDE
+cd IDE
+git clone -b stable https://github.com/HttpAnimation/IDE.git
+mv IDE stable
+git clone -b blank https://github.com/HttpAnimation/IDE.git
+mv IDE blank 
+git clone -b extra https://github.com/HttpAnimation/IDE.git
+mv IDE extra
+git clone -b gh-pages https://github.com/HttpAnimation/IDE.git
+mv IDE gh-pages
+git clone -b gitter https://github.com/HttpAnimation/IDE.git
+mv IDE gitter
+git clone -b main https://github.com/HttpAnimation/IDE.git
+mv IDE main
