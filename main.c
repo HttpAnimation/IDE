3e4175764de495654ce74d648867e7762eca5729
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-07 14:12:32 -0500

    top of file

--- main.c
+++ main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[]) {
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     g_signal_connect(window, "destroy", G_CALLBACK(on_window_closed), NULL);
 
-    // Create a vertical box container for the sidebar and text editor
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+    // Create a grid container for the sidebar and text editor
+    GtkWidget *grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
 
     // Create a label for displaying the file name
     GtkWidget *file_label = gtk_label_new("File: None");
-    gtk_box_pack_start(GTK_BOX(vbox), file_label, FALSE, FALSE, 0);
+    gtk_grid_attach(GTK_GRID(grid), file_label, 0, 0, 1, 1);
 
     // Create a text view for editing code
     GtkWidget *text_view = gtk_text_view_new();
@@ -33,8 +33,8 @@ int main(int argc, char *argv[]) {
                                    GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
 
-    // Add the scrolled window to the vertical box container
-    gtk_box_pack_start(GTK_BOX(vbox), scrolled_window, TRUE, TRUE, 0);
+    // Add the scrolled window to the grid container
+    gtk_grid_attach(GTK_GRID(grid), scrolled_window, 0, 1, 1, 1);
 
     // Show all widgets
     gtk_widget_show_all(window);
